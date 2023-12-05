a2f28bb943ff2f8c7f4ee96135d57715a0262601
Emmanuel Okpunor <okpunorrex@gmail.com>
2023-12-05 13:47:10 +0100

    feature: added svg package

--- App.tsx
+++ App.tsx
@@ -24,6 +24,7 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import Logo from './assets/svgs/trademark.svg';
 
 type SectionProps = PropsWithChildren<{
   title: string;
@@ -72,25 +73,7 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
+        <Logo width={250} height={250} />
       </ScrollView>
     </SafeAreaView>
   );
