a3d5a1d4a7486d2bf1b502c78861b768aa35b09e
Emmanuel Okpunor <okpunorrex@gmail.com>
2023-12-09 11:26:57 +0100

    chore:set up tailwind, redux and rtk query

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,9 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import {Provider} from 'react-redux';
+import {PersistGate} from 'redux-persist/lib/integration/react';
+import {store, persistor} from './store';
 
 import Logo from '../assets/svgs/trademark.svg';
 import Facebook from '../assets/svgs/facebook.svg';
@@ -80,74 +83,79 @@ function App(): JSX.Element {
   // signOut();
 
   return (
-    <SafeAreaView style={{flex: 1}}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <SafeAreaView style={{flex: 1}}>
+          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
 
-      <View
-        style={{
-          flex: 1,
-          paddingHorizontal: 40,
-          alignItems: 'center',
-        }}>
-        <View
-          style={{
-            flex: 0.7,
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '100%',
-          }}>
-          <Logo width={108} height={100} />
-        </View>
-        <View style={{flex: 0.3, width: '100%'}}>
           <View
             style={{
-              flexDirection: 'row',
+              flex: 1,
+              paddingHorizontal: 40,
               alignItems: 'center',
-              gap: 8,
-              justifyContent: 'space-between',
             }}>
-            <View style={styles.line} />
-            <Text>sign up with</Text>
-            <View style={styles.line} />
+            <View
+              style={{
+                flex: 0.7,
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: '100%',
+              }}>
+              <Logo width={108} height={100} />
+            </View>
+            <View style={{flex: 0.3, width: '100%'}}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                  // gap: 8,
+                  // justifyContent: 'space-between',
+                }}
+                className=" justify-between">
+                <View style={styles.line} />
+                <Text>sign up with</Text>
+                <View style={styles.line} />
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-around',
+                  marginTop: 30,
+                }}>
+                <TouchableOpacity
+                  style={{
+                    borderRadius: 15,
+                    borderWidth: 1,
+                    borderColor: '#E8E6EA',
+                    padding: 16,
+                  }}>
+                  <Facebook width={30} height={30} />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  onPress={signIn}
+                  style={{
+                    borderRadius: 15,
+                    borderWidth: 1,
+                    borderColor: '#E8E6EA',
+                    padding: 16,
+                  }}>
+                  <Google width={30} height={30} />
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={{
+                    borderRadius: 15,
+                    borderWidth: 1,
+                    borderColor: '#E8E6EA',
+                    padding: 16,
+                  }}>
+                  <Apple width={30} height={30} />
+                </TouchableOpacity>
+              </View>
+            </View>
           </View>
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-around',
-              marginTop: 30,
-            }}>
-            <TouchableOpacity
-              style={{
-                borderRadius: 15,
-                borderWidth: 1,
-                borderColor: '#E8E6EA',
-                padding: 16,
-              }}>
-              <Facebook width={30} height={30} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={signIn}
-              style={{
-                borderRadius: 15,
-                borderWidth: 1,
-                borderColor: '#E8E6EA',
-                padding: 16,
-              }}>
-              <Google width={30} height={30} />
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={{
-                borderRadius: 15,
-                borderWidth: 1,
-                borderColor: '#E8E6EA',
-                padding: 16,
-              }}>
-              <Apple width={30} height={30} />
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-    </SafeAreaView>
+        </SafeAreaView>
+      </PersistGate>
+    </Provider>
   );
 }
 
