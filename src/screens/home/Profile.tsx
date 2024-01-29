f5e6674a2b0e3f1a259b312f674963d74fa54ba6
Emmanuel Okpunor <okpunorrex@gmail.com>
2024-01-29 21:18:18 +0100

    chore: finished implementation for in app review

--- src/screens/home/Profile.tsx
+++ src/screens/home/Profile.tsx
@@ -1,5 +1,8 @@
 import {View, Text, TouchableOpacity, ScrollView} from 'react-native';
 import React, {useEffect} from 'react';
+import Share from 'react-native-share';
+import InAppReview from 'react-native-in-app-review';
+
 import {
   CustomText,
   Layout,
@@ -9,7 +12,6 @@ import {
 } from '../../components';
 import FastImage from 'react-native-fast-image';
 import {Svg, Navigation} from '../../constants';
-import Share from 'react-native-share';
 
 type Props = {};
 
@@ -28,7 +30,7 @@ const Profile = ({navigation}: any) => {
 
   const handleShare = async () => {
     try {
-      const shareResponse = await Share.open({
+      await Share.open({
         title: 'weMeet',
         message: 'Checkout weMeet chat',
         url: 'https://wemeet.page.link',
@@ -36,6 +38,52 @@ const Profile = ({navigation}: any) => {
     } catch (error) {
       console.log('error with sharing app', error);
     }
+  };
+
+  const handleRateApp = () => {
+    // This package is only available on android version >= 21 and iOS >= 10.3
+
+    // Give you result if version of device supported to rate app or not!
+    InAppReview.isAvailable();
+
+    // trigger UI InAppreview
+    InAppReview.RequestInAppReview()
+      .then(hasFlowFinishedSuccessfully => {
+        // when return true in android it means user finished or close review flow
+        console.log('InAppReview in android', hasFlowFinishedSuccessfully);
+
+        // when return true in ios it means review flow lanuched to user.
+        console.log(
+          'InAppReview in ios has launched successfully',
+          hasFlowFinishedSuccessfully,
+        );
+
+        // 1- you have option to do something ex: (navigate Home page) (in android).
+        // 2- you have option to do something,
+        // ex: (save date today to lanuch InAppReview after 15 days) (in android and ios).
+
+        // 3- another option:
+        if (hasFlowFinishedSuccessfully) {
+          // do something for ios
+          // do something for android
+        }
+
+        // for android:
+        // The flow has finished. The API does not indicate whether the user
+        // reviewed or not, or even whether the review dialog was shown. Thus, no
+        // matter the result, we continue our app flow.
+
+        // for ios
+        // the flow lanuched successfully, The API does not indicate whether the user
+        // reviewed or not, or he/she closed flow yet as android, Thus, no
+        // matter the result, we continue our app flow.
+      })
+      .catch(error => {
+        //we continue our app flow.
+        // we have some error could happen while lanuching InAppReview,
+        // Check table for errors and code number that can return in catch.
+        console.log(error);
+      });
   };
 
   return (
@@ -121,7 +169,7 @@ const Profile = ({navigation}: any) => {
               </View>
             }
             showCaret={false}
-            handlePress={() => {}}
+            handlePress={handleRateApp}
           />
         </View>
         <CustomText as="regular" className="text-center my-4">
